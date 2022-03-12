f966ac93ab052b3d03d77ec34d4398e770a1679f
hirokaissh <hirokai.saito1014@gmail.com>
2022-03-12 16:35:08 +0900

    TableViewDataSorceとTableViewDelegate の設定

--- HIROAKIKadai11/TableViewController.swift
+++ HIROAKIKadai11/TableViewController.swift
@@ -14,4 +14,18 @@ class TableViewController: UIViewController {
 
     }
 
+    @IBAction func cancelButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
+
+extension TableViewController: UITableViewDataSource,UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
 }
