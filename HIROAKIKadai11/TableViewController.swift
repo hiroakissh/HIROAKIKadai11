193934ba7437c2130e807655050f69c72cb2e9a3
hirokaissh <hirokai.saito1014@gmail.com>
2022-03-12 15:52:02 +0900

    ラベルとボタンの配置

---
+++ HIROAKIKadai11/TableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TableViewController.swift
+//  HIROAKIKadai11
+//
+//  Created by HiroakiSaito on 2022/03/12.
+//
+
+import UIKit
+
+class TableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
