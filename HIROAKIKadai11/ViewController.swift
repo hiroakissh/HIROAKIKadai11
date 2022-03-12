4c4aac6050baed61667a5e858f315b00528a634f
hirokaissh <hirokai.saito1014@gmail.com>
2022-03-12 15:54:14 +0900

    ViewControllerとのアウトレット接続

--- HIROAKIKadai11/ViewController.swift
+++ HIROAKIKadai11/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var stateLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
